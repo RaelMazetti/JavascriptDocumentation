84c7f5d300030d465a82f733165f48cbf381903b
Rafael <72637673+Rafae1CM@users.noreply.github.com>
2021-04-01 18:07:13 -0300

    ✨ Random Object in Array

---
+++ Arrays/RandomObjectInArray.js
@@ -0,0 +1,6 @@
+// Random object in Array ✨
+
+let Array = ["One", "Two", "Three"];
+let NumberArray = Math.floor(Math.random() * Array.length);
+
+console.log(`The drawn object was ${Array[NumberArray]}`);
