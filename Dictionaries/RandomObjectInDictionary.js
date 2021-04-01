973e8f63047bacb560e6c5ca4da8ecfa2089a02c
Rafael <72637673+Rafae1CM@users.noreply.github.com>
2021-04-01 18:24:34 -0300

    Delete RandomObjectInDictionary.js

--- Dictionaries/RandomObjectInDictionary.js
+++
@@ -1,9 +0,0 @@
-// Random object in Dictionary ✨
-
-let Dictionary = {
-  Rafael: "Hello Ana!",
-  Ana: "Hello Rafael!"
-};
-let NumberDictionary = Math.floor(Math.random() * Dictionary.length);
-
-console.log(Dictionary[NumberDictionary]); 
