327c96ad9ff290e8f49cde713fd1eb3f7295a8e5
Rafael <72637673+Rafae1CM@users.noreply.github.com>
2021-04-01 18:19:22 -0300

    ✨ Simple Dictionary

---
+++ Dictionaries/SimpleDictionary.js
@@ -0,0 +1,14 @@
+// Simple Dictionary ✨
+
+let Dictionary = {
+  Rafael: {
+    name: "Rafael Cezar Machado",
+    ages: 10 * 10
+  }
+};
+
+function getElementInDictionary (Id) {
+  return Dictionary[Id];
+};
+
+console.log(getElemntInDictionary("Rafael"));
