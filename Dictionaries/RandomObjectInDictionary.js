7391b60ffbe011c8514b6eb0604ea7e5a34fe75a
Rafael <72637673+Rafae1CM@users.noreply.github.com>
2021-04-01 18:23:18 -0300

    Create RandomObjectInDictionary.js

---
+++ Dictionaries/RandomObjectInDictionary.js
@@ -0,0 +1,9 @@
+// Random object in Dictionary ✨
+
+let Dictionary = {
+  Rafael: "Hello Ana!",
+  Ana: "Hello Rafael!"
+};
+let NumberDictionary = Math.floor(Math.random() * Dictionary.length);
+
+console.log(Dictionary[NumberDictionary]);
