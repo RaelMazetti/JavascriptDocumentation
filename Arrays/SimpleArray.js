916e31fd4d0aa4b6889887aa5c114c17886cdf7c
Rafael <72637673+Rafae1CM@users.noreply.github.com>
2021-04-01 18:01:36 -0300

    ✨ Simple Array

---
+++ Arrays/SimpleArray.js
@@ -0,0 +1,11 @@
+// Simple Array ✨
+
+const SimpleArray = ["Rafael", "Cezar", "Machado"];
+//                    [0]        [1]       [2]
+
+for (let Value in SimpleArray) {
+  // Value: Number
+  // SimpleArray[Value]: String
+
+  console.log(SimpleArray[Value]);
+};
